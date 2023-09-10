9a4576e6cb089d509fb041b14890b4b93217d4cb
trueNebula <daniel.ggradinaru@gmail.com>
2023-09-10 10:03:05 +0300

    added auth and pnpm

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,25 +1,24 @@
 import "@styles/globals.css";
 import type { Metadata } from "next";
 import { Providers } from "@components/providers/providers";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-	title: "Login or Sign Up",
-	description: "Change this lmao",
+  title: "Login or Sign Up",
+  description: "Change this lmao",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<ClerkProvider>
-			<html lang="en">
-				<Providers>
-					<body>{children}</body>
-				</Providers>
-			</html>
-		</ClerkProvider>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>
+          <main className="bg-th-background">{children}</main>
+        </Providers>
+      </body>
+    </html>
+  );
 }
